35bfc770b22a846b22555350e65371b11046c5f3
Akash soni <33283321+akashsoni01@users.noreply.github.com>
2026-03-13 22:16:06 +0530

    tc

--- examples/basics_casepath.rs
+++ examples/basics_casepath.rs
@@ -84,26 +84,90 @@ struct DarkStruct {
 
 impl SomeComplexStruct {
     fn new() -> Self {
-        todo!()
-        // Self {
-        //     scsf: Some(SomeOtherStruct {
-        //         sosf: Some(OneMoreStruct {
-        //             omsf: Some(String::from("no value for now")),
-        //             omse: Some(SomeEnum::B(Box::new(DarkStruct {
-        //                 dsf: Some(String::from("dark field")),
-        //             }))),
-        //         }),
-        //     }),
-        //     identity: String::from("SomeComplexStruct"),
-        //     scfs2: Arc::new(std::sync::RwLock::new(SomeOtherStruct {
-        //         sosf: Some(OneMoreStruct {
-        //             omsf: Some(String::from("no value for now")),
-        //             omse: Some(SomeEnum::B(Box::new(DarkStruct {
-        //                 dsf: Some(String::from("dark field")),
-        //             }))),
-        //         }),
-        //     })),
-        // }
+        let inner = SomeOtherStruct {
+            sosf: Box::new(None),
+        };
+
+        Self {
+            scsf: Some(Box::new(inner.clone())),
+            identity: String::from("SomeComplexStruct"),
+
+            // Arc<std::sync::Mutex/RwLock<T>>
+            scfs2: Arc::new(std::sync::Mutex::new(inner.clone())),
+            scfs3: Arc::new(std::sync::RwLock::new(inner.clone())),
+
+            // Option<std::sync::Mutex/RwLock<T>>
+            scfs6: Some(std::sync::Mutex::new(inner.clone())),
+            scfs7: Some(std::sync::RwLock::new(inner.clone())),
+
+            // Option<parking_lot::Mutex/RwLock<T>>
+            scfs8: Some(Mutex::new(inner.clone())),
+            scfs9: Some(RwLock::new(inner.clone())),
+
+            // std::sync::Mutex/RwLock<Option<T>>
+            scfs10: std::sync::Mutex::new(Some(inner.clone())),
+            scfs11: std::sync::RwLock::new(Some(inner.clone())),
+
+            // parking_lot::Mutex/RwLock<Option<T>>
+            scfs12: Mutex::new(Some(inner.clone())),
+            scfs13: RwLock::new(Some(inner.clone())),
+
+            // Option<std::sync::Mutex/RwLock<Option<T>>>
+            scfs14: Some(std::sync::Mutex::new(Some(inner.clone()))),
+            scfs15: Some(std::sync::RwLock::new(Some(inner.clone()))),
+
+            // Option<parking_lot::Mutex/RwLock<Option<T>>>
+            scfs16: Some(Mutex::new(Some(inner.clone()))),
+            scfs17: Some(RwLock::new(Some(inner.clone()))),
+
+            // Locks inside Arc
+            scfs_arc_pl_m: Arc::new(Mutex::new(inner.clone())),
+            scfs_arc_pl_rw: Arc::new(RwLock::new(inner.clone())),
+            scfs_arc_std_mo: Arc::new(std::sync::Mutex::new(Some(inner.clone()))),
+            scfs_arc_std_rwo: Arc::new(std::sync::RwLock::new(Some(inner.clone()))),
+            scfs_arc_pl_mo: Arc::new(Mutex::new(Some(inner.clone()))),
+            scfs_arc_pl_rwo: Arc::new(RwLock::new(Some(inner.clone()))),
+
+            // Option<Arc<...>>
+            scfs_o_arc_std_m: Some(Arc::new(std::sync::Mutex::new(inner.clone()))),
+            scfs_o_arc_std_rw: Some(Arc::new(std::sync::RwLock::new(inner.clone()))),
+            scfs_o_arc_pl_m: Some(Arc::new(Mutex::new(inner.clone()))),
+            scfs_o_arc_pl_rw: Some(Arc::new(RwLock::new(inner))),
+
+            // Tokio fields (only when feature is enabled)
+            #[cfg(feature = "tokio")]
+            scfs_t_arc_m: Arc::new(tokio::sync::Mutex::new(SomeOtherStruct {
+                sosf: Box::new(None),
+            })),
+            #[cfg(feature = "tokio")]
+            scfs_t_arc_rw: Arc::new(tokio::sync::RwLock::new(SomeOtherStruct {
+                sosf: Box::new(None),
+            })),
+            #[cfg(feature = "tokio")]
+            scfs_t_arc_mo: Arc::new(tokio::sync::Mutex::new(Some(SomeOtherStruct {
+                sosf: Box::new(None),
+            }))),
+            #[cfg(feature = "tokio")]
+            scfs_t_arc_rwo: Arc::new(tokio::sync::RwLock::new(Some(SomeOtherStruct {
+                sosf: Box::new(None),
+            }))),
+            #[cfg(feature = "tokio")]
+            scfs_t_o_arc_m: Some(Arc::new(tokio::sync::Mutex::new(SomeOtherStruct {
+                sosf: Box::new(None),
+            }))),
+            #[cfg(feature = "tokio")]
+            scfs_t_o_arc_rw: Some(Arc::new(tokio::sync::RwLock::new(SomeOtherStruct {
+                sosf: Box::new(None),
+            }))),
+            #[cfg(feature = "tokio")]
+            scfs_t_o_arc_mo: Some(Arc::new(tokio::sync::Mutex::new(Some(SomeOtherStruct {
+                sosf: Box::new(None),
+            })))),
+            #[cfg(feature = "tokio")]
+            scfs_t_o_arc_rwo: Some(Arc::new(tokio::sync::RwLock::new(Some(SomeOtherStruct {
+                sosf: Box::new(None),
+            })))),
+        }
     }
 }
 fn main() {
@@ -128,13 +192,22 @@ fn main() {
     });
     
     // LockKp for Arc<parking_lot::RwLock<Option<SomeOtherStruct>>> should yield value SomeOtherStruct
-    let x_pl_rw: Option<&SomeOtherStruct> = SomeComplexStruct::scfs_arc_pl_rwo().get(todo!());
+    let x_pl_rw: Option<&SomeOtherStruct> =
+        SomeComplexStruct::scfs_arc_pl_rwo().get(&instance);
     // Same for Arc<std::sync::Mutex<Option<SomeOtherStruct>>>
-    let x_std_m: Option<&SomeOtherStruct> = SomeComplexStruct::scfs_arc_std_mo().get(todo!());
+    let x_std_m: Option<&SomeOtherStruct> =
+        SomeComplexStruct::scfs_arc_std_mo().get(&instance);
     // And Arc<std::sync::RwLock<Option<SomeOtherStruct>>>
-    let x_std_rw: Option<&SomeOtherStruct> = SomeComplexStruct::scfs_arc_std_rwo().get(todo!());
+    let x_std_rw: Option<&SomeOtherStruct> =
+        SomeComplexStruct::scfs_arc_std_rwo().get(&instance);
     // And Arc<parking_lot::Mutex<Option<SomeOtherStruct>>>
-    let x_pl_m: Option<&SomeOtherStruct> = SomeComplexStruct::scfs_arc_pl_mo().get(todo!());
+    let x_pl_m: Option<&SomeOtherStruct> =
+        SomeComplexStruct::scfs_arc_pl_mo().get(&instance);
+
+    assert!(x_pl_rw.is_some());
+    assert!(x_std_m.is_some());
+    assert!(x_std_rw.is_some());
+    assert!(x_pl_m.is_some());
     // if let Some(omsf) = SomeComplexStruct::scfs2_lock()
     //     .then(SomeOtherStruct::sosf())
     //     .then(OneMoreStruct::omse())
